1409ff9b566a32383b8014841e0ac6ccfc8a1eb2
Ricardo Quintanar <lrq1012@gmail.com>
2019-04-03 07:33:36 -0500

    code is working again, need to review information being pulled

--- liri.js
+++ liri.js
@@ -1,32 +1,31 @@
 // require("dotenv").config();
 
 
-var axios = require ("axios");
-var fs = require ("fs");
+var axios = require("axios");
+var fs = require("fs");
 var Spotify = ("node-spotify-api");
 var keys = require("./keys.js")
+var bandKey = keys.bandKey.apikey
 
 
+var Liri = function () {
+  var divider = "\n------------------------------------------------------------\n\n";
 
- var Liri = function () {
-    var divider = "\n------------------------------------------------------------\n\n";
-  
-    this.findArtist = function (artist) {
-        var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-  
-      axios.get(URL).then(function (response) {
-        var jsonData = response.data;
-        console.log(jsonData)
-        var artistData = [
-          "Venue: " + jsonData.Venue,
-          "Location " + jsonData.Location,
-          "Date: " + jsonData.Date,
-        ].join("\n\n");
-          fs.appendFile("log.txt", artistData + divider, function (err) {
-            if (err) throw err;
-            console.log(artistData);
-          });
+  this.findArtist = function (artist) {
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(URL).then(function (response) {
+      var jsonData = response.data;
+      console.log(jsonData)
+      var artistData = [
+        "Venue: " + jsonData.Venue,
+        "Location " + jsonData.Location,
+        "Date: " + jsonData.Date,
+      ].join("\n\n");
+      fs.appendFile("log.txt", artistData + divider, function (err) {
+        if (err) throw err;
+        console.log(artistData);
       });
-    };
+    });
   };
-  module.exports = Liri;
+};
+module.exports = Liri;
--- liriapp.js
+++ liriapp.js
@@ -3,16 +3,13 @@ var Liri = require("./liri")
 var liri = new Liri();
 var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
-// for (var i = 4; i < process.argv.length; i++) {
-//     term += (" " + process.argv[i]);
-// }
 
 if (!search) {
-  search = "artist";
+  search = "concert-this";
 }
 
 if (!term) {
-  term = "Aerosmith";
+  term = "drake";
 }
 
 if (search === "concert-this") {
